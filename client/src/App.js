797152cee55781145253fd6d275e371e22f1ee5d
ofir <ofirsimhi1612@gmail.com>
2020-11-29 17:42:20 +0200

    search component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Search from './components/search'
+
+function App() {
+  return (
+    <div className="App">
+      <Search></Search>
+    </div>
+  );
+}
+
+export default App;
