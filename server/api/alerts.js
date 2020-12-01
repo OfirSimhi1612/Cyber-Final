b917b51c21f156d9fd0e1b841859ff4a749a438d
ofir <ofirsimhi1612@gmail.com>
2020-12-01 16:10:00 +0200

    api in node server

--- server/api/alerts.js
+++ server/api/alerts.js
@@ -1,13 +1,16 @@
 const { Router } = require('express')
+const axios = require('axios')
 
 const router = Router()
 
 const alertsHost = process.env.ALERTS_HOST || 'localhost'
 const alertsPort = process.env.ALERTS_PORT || '3001'
 
+const alertsURL = (entry) =>  `http://${alertsHost}:${alertsPort}/alerts/${entry}`
+
 router.get('/:user', async (req, res) => {
     try{
-        
+        const {data: alerts} = await axios.get(alertsURL(req.params.user))
         
         res.send(alerts)
     } catch(err){
@@ -16,12 +19,26 @@ router.get('/:user', async (req, res) => {
     }
 })
 
-router.post('/read', async (req, res) => {
-
+router.post('/read/:_id', async (req, res) => {
+    try{
+        const {data: alerts} = await axios.post(alertsURL(`read/${req.params._id}`))
+        
+        res.send(alerts)
+    } catch(err){
+        console.log(err)
+        res.status(500).send('error')
+    }
 })
 
-router.delete('/', async (req, res) => {
-
+router.delete('/:_id', async (req, res) => {
+    try{
+        const {data: alerts} = await axios.delete(alertsURL(req.params._id))
+        
+        res.send(alerts)
+    } catch(err){
+        console.log(err)
+        res.status(500).send('error')
+    }
 })
 
 module.exports = router
